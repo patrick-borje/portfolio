50b82a0d9be738b633a6537eec6932d528aad38d
Patrick Borje <borje.pb@gmail.com>
2020-02-26 17:25:49 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9e206d62d7c450c14b2a43d9e7d5b259.js"
+  "/portfolio/precache-manifest.7e43a0f61e4686a476e1eaaf80ab9bd8.js"
 );
 
 self.addEventListener('message', (event) => {
