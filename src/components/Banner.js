683440b9c780bdee34294e3dcc6e9c40370fb693
Patrick Borje <borje.pb@gmail.com>
2020-02-26 17:16:18 +0800

    changed content

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,12 +7,20 @@ function Banner() {
       <div className="introduction">
         <div className="intro-text">
           <h1 className="hello">HELLO,</h1>
-          <h1 className="name">I'M PATRICK BORJE</h1>
+          <h1 className="name">I'M PATRICK</h1>
         </div>
       </div>
       <div class="about">
         <h2>About Me</h2>
-        <p>Hello I'm Patrick Borje</p>
+        <p>
+          I graduated form Phinma University Of Pangasinan with a critical
+          experience in learning technology. I can implement effective and user
+          friendly websites which enables me to help and satisfy clients.
+        </p>
+        <p>
+          Striving to leverage my web development skills especially front end
+          development.
+        </p>
       </div>
     </section>
   );
