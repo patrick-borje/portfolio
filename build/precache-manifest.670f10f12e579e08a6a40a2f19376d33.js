3b0aa678fecc03d0d089c14aa4ff5890804fdda8
Patrick Borje <borje.pb@gmail.com>
2020-02-26 16:34:34 +0800

    second commit

---
+++ build/precache-manifest.670f10f12e579e08a6a40a2f19376d33.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50445b06d1dfa8966823b1992a7d665",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d01c1110550e9b8854f7",
+    "url": "/portfolio/static/css/main.ef7ff537.chunk.css"
+  },
+  {
+    "revision": "e67dcb9b7d7225d6705c",
+    "url": "/portfolio/static/js/2.e83d53f4.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.e83d53f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d01c1110550e9b8854f7",
+    "url": "/portfolio/static/js/main.55a8e636.chunk.js"
+  },
+  {
+    "revision": "d89ad7dd13ac8d86cc0e",
+    "url": "/portfolio/static/js/runtime-main.94c268e8.js"
+  },
+  {
+    "revision": "fa90f8c6f63a92385128e5a09e004c4f",
+    "url": "/portfolio/static/media/Capture.fa90f8c6.PNG"
+  },
+  {
+    "revision": "a06f3bd046a5091d43110cd6f8ba4775",
+    "url": "/portfolio/static/media/pic-art.a06f3bd0.PNG"
+  }
+]);
